59019a8b87afc678eeb6f4d10a62158dddd7af56
Felipe Cicolin Rocha <felipecicolinrocha@gmail.com>
2022-06-15 21:30:23 -0300

    Registers nami controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import NamiController from "./nami_controller"
+application.register("nami", NamiController)
